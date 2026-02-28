e0de79e601d7675bde7c7279548372e1e18a79b3
Jihong Min <jhjihong@naver.com>
2026-02-28 21:22:44 +0900

    Add support for CannonKeys Type-B

---
+++ keyboards/cannonkeys/typeb/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xF8, 0x3B, 0x63, 0xAD, 0x98, 0x84, 0x18, 0x1A}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
